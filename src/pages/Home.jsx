b78766d73350a541813b308543b0435c223c978e
Adisie Fenta <adisiefenta23@gmail.com>
2024-04-28 11:51:20 +0300

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,26 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="max-w-[1200px] px-3 mx-auto">
+        <header>
+          <h1 className="text-lg font-medium text-gray-700 w-max underline uppercase">
+            1.Line Charts
+          </h1>
+        </header>
+        <div>
+          {/* basic charts */}
+          <header className="my-3">
+            <h3 className="font-bold text-gray-700">1.1 Basic Line Charts</h3>
+          </header>
+          <div className="grid grid-cols-3 gap-5">
+            <div>one</div>
+            <div>two</div>
+            <div>three</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
